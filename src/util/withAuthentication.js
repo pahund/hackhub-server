314e05ce4d5ea462a2fb87bc955e2686bb33cb5c
Patrick Hund <pahund@team.mobile.de>
2016-06-10 10:18:19 +0200

    HTTPS only
    Change-Id: I3b1d8bed121302034d418463d4a5f7a2f2b92808

--- src/util/withAuthentication.js
+++ src/util/withAuthentication.js
@@ -7,15 +7,19 @@
 const credentials = require("../../credentials.json");
 
 module.exports = func => (req, res) => {
+    if (req.hostname !== "localhost" && req.protocol !== "https") {
+        res.status(403).json({ error: "SSL access only" });
+        return;
+    }
     if (!req.headers.authorization) {
-        res.json({ error: "No credentials sent"});
+        res.status(401).json({ error: "No credentials sent"});
         return;
     }
     const encoded = req.headers.authorization.split(' ')[1];
     const decoded = new Buffer(encoded, "base64").toString("utf8");
     const [ id, secret ] = decoded.split(":");
     if (credentials.id !== id || credentials.secret !== secret) {
-        res.json({ error: "Incorrect credentials" });
+        res.status(401).json({ error: "Invalid credentials" });
         return;
     }
     func(req, res);
